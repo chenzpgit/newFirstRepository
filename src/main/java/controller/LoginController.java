e49496b753b8ba27c169f880ad6a25e44f3e7506
chenzhiping <Chenzp&9149002266>
2018-11-06 11:31:16 +0800

    登陆控制器

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -2,9 +2,13 @@ package controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @RequestMapping(value = "login")
 public class LoginController {
 
+
+    public Map<String,String> longin(){return  null;}
 
 
 }
