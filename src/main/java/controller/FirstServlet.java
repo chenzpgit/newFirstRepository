4e491d33855f1e369215f4d8b376e62efce35112
chenzhiping <Chenzp&9149002266>
2018-11-06 10:19:01 +0800

    新建项目

---
+++ src/main/java/controller/FirstServlet.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FirstServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("get方法+");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.print("你好，   Hello World");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       doGet(req,resp);
+    }
+}
